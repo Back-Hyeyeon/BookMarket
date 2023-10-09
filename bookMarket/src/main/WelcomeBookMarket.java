ca040ad4f9387c46bee921537a1dececaf781574
Back Hyeyeon <skwir21@gmail.com>
2023-10-10 02:08:16 +0900

    [create Bill] bill make

--- bookMarket/src/main/WelcomeBookMarket.java
+++ bookMarket/src/main/WelcomeBookMarket.java
@@ -1,12 +1,12 @@
 package main;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class WelcomeBookMarket {
 	static final int NUM_BOOK = 3; // 도서 개수
 	static final int NUM_ITEM = 8; // 도서 정보의 개수
-	static CartItem[] cartItem = new CartItem[NUM_BOOK]; // 장바구니
-	static int cartCount = 0; // 장바구니 도서 개수
 
 	static Cart cart = new Cart();
 	static User user;
@@ -28,14 +28,6 @@ public class WelcomeBookMarket {
 
 		boolean quit = false;
 		while (!quit) {
-//			System.out.println("***************************************************");
-//			System.out.println("\t\t" + "Book Market Menu");
-//			System.out.println("***************************************************");
-//			System.out.println(" 1. 고객 정보 확인하기 \t4. 장바구니에 항목 추가하기");
-//			System.out.println(" 2. 장바구니 상품 목록 보기\t5. 장바구니의 항목 수량줄이기");
-//			System.out.println(" 3. 장바구니 비우기 \t6. 장바구니의 항목 삭제하기");
-//			System.out.println(" 7. 영수증 표시하기 \t8. 종료");
-//			System.out.println("***************************************************");
 
 			menuIntroduction();
 
@@ -47,8 +39,6 @@ public class WelcomeBookMarket {
 				switch (numberSelection) {
 				case 1:
 					menuGuestInfo(userName, userMobile);
-//					System.out.println("현재 고객 정보");
-//					System.out.printf("이름 : %s , 연락처 : %d \n", userName, userMobile);
 					break;
 				case 2:
 					menuCartItemList();
@@ -88,7 +78,6 @@ public class WelcomeBookMarket {
 		}
 	}
 
-
 	private static void menuAdminLogin() {
 		System.out.println("관리자 정보를 입력하세요");
 		Scanner input = new Scanner(System.in);
@@ -110,8 +99,32 @@ public class WelcomeBookMarket {
 	}
 
 	private static void menuCartBill() {
-		System.out.println("7. 영수증 표시하기");
+		// System.out.println("7. 영수증 표시하기");
+		if (cart.cartCount == 0) {
+			System.out.println("장비구니에 항목이 없습니다");
+		} else {
+			System.out.println("배송받을 분은 고객정보와 같습니까? Y | N ");
+			Scanner input = new Scanner(System.in);
+			String str = input.nextLine();
+			if (str.toUpperCase().equals("Y") || str.toUpperCase().equals("y")) {
+				System.out.print("배송지를 입력해주세요 ");
+				String address = input.nextLine();
+				// 주문 처리 후 영수증 출력 메서드 호출
+				printBill(user.getName(), String.valueOf(user.getPhone()), address);
+			} else {
+				System.out.print("배송받을 고객명을 입력하세요 ");
+				String name = input.nextLine();
+				System.out.print("배송받을 고객의 연락처를 입력하세요 ");
+				String phone = input.nextLine();
+				System.out.print("배송받을 고객의 배송지를 입력해주세요 ");
+				String address = input.nextLine();
+				// 주문 처리 후 영수증 출력 메서드 호출
+				printBill(name, phone, address);
+			}
+		}
+
 	}
+
 
 	private static void menuCartRemoveItem() {
 //		System.out.println("6. 장바구니의 항목 삭제하기");
@@ -156,11 +169,6 @@ public class WelcomeBookMarket {
 	private static void menuCartAddItem(Book[] book) {
 //		System.out.println("4. 장바구니에 항목 추가하기 : ");
 		bookList(book);// 도서 정보메서드 출력
-//		for (int i = 0; i < NUM_BOOK; i++) {
-//			for (int j = 0; j < NUM_ITEM; j++)
-//				System.out.print(book[i][j] + " | ");
-//			System.out.println("");
-//		}
 		cart.printBookList(book);
 		boolean quit = false;
 
@@ -193,8 +201,6 @@ public class WelcomeBookMarket {
 		}
 	}
 
-
-
 	private static void menuCartClear() {
 //		System.out.println("3. 장바구니 비우기");
 		if (cart.cartCount == 0) {
@@ -212,23 +218,13 @@ public class WelcomeBookMarket {
 	}
 
 	private static void menuCartItemList() {
-		System.out.println("2. 장바구니 상품 목록 보기 :");
-		System.out.println("---------------------------------------------");
-		System.out.println(" 도서 ID \t| 수량 \t| 합계");
-		for (int i = 0; i < cartCount; i++) {
-			System.out.print(" " + cartItem[i].getBookID() + " \t| ");
-			System.out.print(" " + cartItem[i].getQuantity() + " \t| ");
-			System.out.print(" " + cartItem[i].getTotalPrice());
-			System.out.println(" ");
+		if (cart.cartCount >= 0) {
+			cart.printCart();
 		}
-		System.out.println("---------------------------------------------");
 	}
 
 	private static void menuGuestInfo(String userName, int userMobile) {
 		System.out.println("현재 고객 정보");
-//		System.out.printf("이름 : %s , 연락처 : %d \n", userName, userMobile);
-//		Customer customer = new Customer(name, phone);
-//		System.out.println("이름 : " + customer.getName() + ", 연락처 : " + customer.getPhone());
 		System.out.printf("이름 : %s  , 연락처 : %d \n", user.getName(), user.getPhone());
 
 	}
@@ -246,25 +242,23 @@ public class WelcomeBookMarket {
 	}
 
 	private static void bookList(Book[] book) {
-		book[0] = new Book("book1", "ISBN 978-89-01-26726-5", "빅 히스토리",
-				33000);
-				book[0].setAuthor("데이비드 크리스천");
-				book[0].setDescription("우주와 지구, 인간을 하나로 잇는 새로운 역사");
-				book[0].setCategory("인문 교양");
-				book[0].setReleaseDate("2022/12/23");
-				book[1] = new Book("book2","ISBN 979-11-6921-062-1","SICP", 45000);
-				book[1].setAuthor("해럴드 에이블슨, 류광");
-				book[1].setDescription("컴퓨터 프로그래밍의 구조와 해석");
-				book[1].setCategory("개발 방법론");
-				book[1].setReleaseDate("2022/12/30");
-				book[2] = new Book("book3", "ISBN 978-89-6626-366-0", "러스트 프로그래밍",35000);
-				book[2].setAuthor("팀 맥나마라, 장연호");
-				book[2].setDescription("러스트는 시스템 프로그래밍에 적합한 언어");
-				book[2].setCategory("프로그래밍 언어");
-				book[2].setReleaseDate("2022/07/08");
+		book[0] = new Book("book1", "ISBN 978-89-01-26726-5", "빅 히스토리", 33000);
+		book[0].setAuthor("데이비드 크리스천");
+		book[0].setDescription("우주와 지구, 인간을 하나로 잇는 새로운 역사");
+		book[0].setCategory("인문 교양");
+		book[0].setReleaseDate("2022/12/23");
+		book[1] = new Book("book2", "ISBN 979-11-6921-062-1", "SICP", 45000);
+		book[1].setAuthor("해럴드 에이블슨, 류광");
+		book[1].setDescription("컴퓨터 프로그래밍의 구조와 해석");
+		book[1].setCategory("개발 방법론");
+		book[1].setReleaseDate("2022/12/30");
+		book[2] = new Book("book3", "ISBN 978-89-6626-366-0", "러스트 프로그래밍", 35000);
+		book[2].setAuthor("팀 맥나마라, 장연호");
+		book[2].setDescription("러스트는 시스템 프로그래밍에 적합한 언어");
+		book[2].setCategory("프로그래밍 언어");
+		book[2].setReleaseDate("2022/07/08");
 
 	}
-	
 
 	private static boolean isCartInBook(String bookId) {
 //		boolean flag = false;
@@ -277,4 +271,24 @@ public class WelcomeBookMarket {
 //		return flag;
 		return cart.isCartInBook(bookId);
 	}
-}
+	private static void printBill(String name, String phone, String address) {
+		Date date = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+		String strDate = formatter.format(date);
+		System.out.println();
+		System.out.println("---------------배송 받을 고객 정보----------------");
+		System.out.println("고객명 : " + name + " \t\t 연락처 : " + phone);
+		System.out.println("배송지 : " + address + "\t 발송일 : " + strDate);
+		// 장바구니에 담긴 항목 출력
+		cart.printCart();
+		// 장바구니에 담긴 항목의 총 금액 계산
+		int sum = 0;
+		for (int i = 0; i < cart.cartCount; i++) {
+		sum += cart.cartItem[i].getTotalPrice();
+		}
+		System.out.println("\t\t\t 주문 총금액 : " + sum + "원\n");
+		System.out.println("----------------------------------------------");
+		System.out.println();
+		}
+	}
+
